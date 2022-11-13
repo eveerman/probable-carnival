838aa9d233544adbf95a99f6f2ad05187f8c9849
eveerman <erik@veerman.tech>
2022-11-13 13:15:49 +0000

    wasting time pulling files!

--- read_secdef.py
+++ read_secdef.py
@@ -1,6 +1,9 @@
 import sys
 from os import path
 import argparse
+import ftplib
+import gzip 
+import shutil
 
 def getArgs():
     parser = argparse.ArgumentParser()
@@ -13,13 +16,31 @@ def getArgs():
     secdef_file = arguments.secdef_file
     return secdef_file
 
+def retrieveSecdef():
+    host = 'ftp.cmegroup.com'
+    #secdef_path = "SBEFix/Production/secdef.dat.gz"
+    filename = 'secdef.dat.gz'
+    file = open(filename, 'wb')
+    server = ftplib.FTP(host)
+    server.login()
+    #server.retrbinary('RETR {secdef_path}', file.write)
+    server.retrbinary('RETR SBEFix/Production/secdef.dat.gz', file.write)
+    print("File downloaded, unpacking ...")
+    with gzip.open('secdef.dat.gz', 'rb') as unpack:
+        with open('secdef.dat', 'wb') as unpacked:
+            shutil.copyfileobj(unpack, unpacked)
+    server.quit()
+            
 def checkFile(secdef_file):
     exit = False
     input_file_exists = path.exists(secdef_file)
     if not input_file_exists:
         print("Input File does not exists, attempt download ...")
-
-        exit = True
+        retrieveSecdef()
+        input_file_exists = path.exists(secdef_file)
+        if not input_file_exists:
+            print("I've borked something :(")
+            exit = True
     if exit == True:
         sys.exit()
 
@@ -27,6 +48,9 @@ def main():
     secdef_file = getArgs()
     checkFile(secdef_file)
     print("FINISHED")
+    #with open(secdef_file) as file:
+    #    first5 = [next(file) for x in range(5)]
+    #print(first5)
 
 if __name__ == '__main__':
     main()
