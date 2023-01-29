028916899d460e159df0a891bee321dcda0255c1
eveerman <erik@veerman.tech>
2023-01-29 18:08:02 +0000

    remove testing, pointless prints, unused etc

--- read_secdef.py
+++ read_secdef.py
@@ -63,22 +63,9 @@ def startParsing(secdef_file):
         '555' : 'NoLegs' #legs (spreads)
     }
 
-    #with open(secdef_file) as file:
-    #    first5 = [next(file) for x in range(5)]
-    #print(first5)
-
     with open(secdef_file) as file:
         all_secdef = file.read()
     all_secdef = [l for l in all_secdef.split('\n') if l]
-    #print(all_secdef[0].split('\x01'))
-    #listy = []
-    #count = 0
-
-    #for ln in all_secdef:
-    #    listy += '='.join(ln.strip('\x01').split('\x01')).split('=')
-    #print(type(listy))
-    #rint(listy[:10])
-    
     data = {}
     i = 0
     for ln in all_secdef:
@@ -86,35 +73,24 @@ def startParsing(secdef_file):
         oneln = iter(item)
         data[i] = dict((key,value) for key, value in zip(oneln, oneln) if key in tags_map.keys())
         i += 1
-    print(item)
-    #print(type(data))
-    print(data[0])
-    #total = len(data)
-    #print(data)
     security = []
     products = []
     ge = []
-    #print
     for item in data:
         security += {data[item]['167']}
         if 'FUT' in {data[item]['167']}:
             if '462' in data[item]:
                 products.append(data[item]['462'])
             if 'GE' == data[item]['6937']:
-                #print(data[item])
                 if '555' not in data[item]:
-                    #print(data[item])
                     appendge= {}
                     appendge["name"] = data[item]['55']
                     appendge["expiry"] = data[item]['200']
                     ge.append(appendge)
                     del appendge
-
     ## for future: learn to use pandas :/
 
-    #print(security[:5])
     securities = []
-
     for x in security:
         if x not in securities:
             securities.append(x)
@@ -122,37 +98,17 @@ def startParsing(secdef_file):
         print(x)
         print(security.count(x))
 
-    list_products = set(products)
-    #print(list(list_products))
     for x in set(products):
         print(x)
         print(products.count(x))
 
-    #print(ge)
     ge.sort(key = lambda x: datetime.strptime(x['expiry'], '%Y%m'))
     for x in ge[:4]:
         print(f'{x}')
 
-    #print(appendge)
-    #    print(products.count(x))
-    #keys = {}
-    #values = {}
-    #for key, nested in data.iteritems():
-    #    for 
-    #values_per_key = {}
-    #for item in data:
-    #    for k, v in item.items():
-    #        values_per_key.setdefault(k, set()).add(v)
-    #counts = {k: len(v) for k, v in values_per_key.items()}
-    #counts
-    #print(data)
-
-
-
 def main():
     secdef_file = getArgs()
     checkFile(secdef_file)
-    #print("FINISHED")
     startParsing(secdef_file)
     
     print("COMPLETE")
